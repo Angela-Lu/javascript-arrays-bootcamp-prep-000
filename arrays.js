09a9ca1d767d7630337a21e1f72bdf0278b8b2f3
Angela Lu <angela.lu0919@gmail.com>
2018-08-09 03:33:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ return  array.slice(1);
 
 function destructivelyRemoveElementFromEndOfArray(array) {
  array.pop();
- return array()
+ return array
 }
 
 function removeElementFromEndOfArray(array) {
